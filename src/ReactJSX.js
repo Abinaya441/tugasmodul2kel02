6715ee411b0e2436ac01dc0cedce574897eed95a
Nurhaidah <63896736+nurhaidah346@users.noreply.github.com>
2022-10-22 20:51:53 +0700

    Create ReactJSX.js

---
+++ src/ReactJSX.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ReactJSX = () => {
+    return (
+        <div>
+            <h1>Tugas Modul 2 Praktikum RPLBK Kelompok 02</h1>
+            <p>Defin Friko Fahdani 21120119130054</p>
+            <p>Muhammad Abinaya Isaqofi 21120119130039</p>
+            <p>Ega Oktabrianto 21120119130066</p>
+            <p>Nurhaidah 21120119120003</p>   
+        </div>
+    );
+}
+
+export default ReactJSX;
