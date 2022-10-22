a482997e6081a2a9bfed07143072db148e68798f
Pointdownz0 <egainsanity@gmail.com>
2022-10-22 18:35:12 +0700

    semua Table ini tipuan aizen

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       {/* <header className="App-header"> */}
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Tugas Modul 2 Praktikum RPLBK Kelompok 02</h1>
         <p>Defin Friko Fahdani 21120119130054</p>
         <p>Muhammad Abinaya Isaqofi 21120119130039</p>
         <p>Ega Oktabrianto 21120119130066</p>
         <p>Nurhaidah 21120119120003</p>
+
         <input
           type="text"
           value={kuery}
@@ -25,13 +25,25 @@ function App() {
           data.filter(post => {
             if (kuery === '') {
               return post;
-            } else if (post.nim.toString().includes(kuery.toString())) {
+            } else if (post.nim.toString() === kuery.toString()) {
               return post;
             }
           }).map((post) => (
             <div className="box" key={post.nim}>
-              <p>{post.nama_lengkap}</p>
-              <p>{post.nama_panggilan}</p>
+              
+              <table border={0}>
+                <div className="img-profile">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6FoyrD5xNNMboCYW6q0zwT-KjEvkBnta5Zfh2Dqata3a_1ZpK62EVFsnj_1cyca5aprI&usqp=CAU"></img>
+                 </div>
+                <tr><td>Nama Lengkap</td> <td> - </td> <td>{post.nama_lengkap}</td> </tr>
+                <tr><td>Nama Panggilan</td> <td> - </td> <td>{post.nama_panggilan}</td> </tr>
+                <tr><td>No Telp.</td> <td> - </td> <td>{post.nomor_telepon}</td> </tr>
+                <tr><td>ID LINE</td> <td> - </td> <td>{post.id_line}</td> </tr>
+                <tr><td>Tanggal Lahir</td> <td> - </td> <td>{post.tanggal_lahir}</td> </tr>
+                <tr><td>NIM</td> <td> - </td> <td>{post.nim}</td> </tr>
+                <tr><td>Email</td> <td> - </td> <td>{post.email}</td> </tr>
+                <tr><td>Hobi</td> <td> - </td> <td>{post.hobi}</td> </tr>
+                </table>
             </div>
           ))
         }
